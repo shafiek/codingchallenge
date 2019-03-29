122f3e4f3c3a39ff62a8e2cf6350bd898b8003c6
Shafiek Savahl <shafiek.savahl@auspost.com.au>
2019-03-29 21:07:34 +1100

    Added PMD and fixed some checkstyle rules

--- src/main/java/com/savahl/mebank/codechallenge/App.java
+++ src/main/java/com/savahl/mebank/codechallenge/App.java
@@ -2,11 +2,21 @@ package com.savahl.mebank.codechallenge;
 
 public class App {
 
+    /**
+     * This is a test method.
+     * @return a value
+     */
     public String getGreeting() {
         return "Hello world.";
     }
 
+    /**
+     * This is a main method.
+     * @param args arguments
+     */
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
+
+
     }
 }
